f05440bd8578415c1d70a1157012c4aa1bd028e8
Byakk0_ <85170351+ByakkO3@users.noreply.github.com>
2021-10-19 13:42:48 +0200

    Create Files_Size_Count.ps1

---
+++ PowerShell Scripts/Files_Size_Count.ps1
@@ -0,0 +1,8 @@
+$path=C:\
+$colItems = Get-ChildItem $path | Where-Object {$_.PSIsContainer -eq $true} | Sort-Object
+foreach ($i in $colItems){
+    $subFolderItems = Get-ChildItem $i.FullName -recurse -force | Where-Object {$_.PSIsContainer -eq $false} | Measure-Object -property Length -sum | Select-Object Sum
+    $i.FullName + " -- " + "{0:N2}" -f ($subFolderItems.sum / 1MB) + " MB"
+    $count=(Get-ChildItem -Recurse $path$i).Count
+    Write-Host "Files in $i : $count";""
+}
