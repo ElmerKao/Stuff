573f7c06e4c3ecbf2ce0cf3b3c145c1167402dba
ElmerKao_ <85170351+ElmerKao@users.noreply.github.com>
2021-11-23 23:36:31 +0100

    Delete PowerShell Scripts directory

--- PowerShell Scripts/Files_Size_Count.ps1
+++
@@ -1,7 +0,0 @@
-gci -force 'C:\'-ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
-$len = 0
-$cont=(Get-ChildItem -Force -Recurse -ErrorAction SilentlyContinue  "C:\$_").count
-gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
-$_.fullname, '{0:N2} MB' -f ($len / 1mb)
-Write-host " * Files and Directories in $_ $cont";""
-}
--- PowerShell Scripts/README.md
+++
@@ -1,3 +0,0 @@
-This Files are some stuff with Powershell I do for Windows 👨‍💻
-
-- Each name of file explains what it does beacuse they aren't much complex
